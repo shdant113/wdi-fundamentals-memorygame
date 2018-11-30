6bd3c2392f01c9024a4148c36759aff814955a26
shdant113 <45294386+shdant113@users.noreply.github.com>
2018-11-30 12:57:17 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,69 @@
 console.log("Up and running!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardFour);
+
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}];
+
+var cardsInPlay = [];
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
+}
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+		this.setAttribute('src', cards[cardId].cardImage);
+		if (cardsInPlay.length === 2);
+	checkForMatch();
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+}
+
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {	
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard)
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
+
+
+
+// var cardOne = cards[0]; {
+//	cardsInPlay.push(CardOne);
+//	console.log("User flipped" + cardOne);
+// }
+//	var cardThree = cards[2]; {
+//	cardsInPlay.push(CardTwo);
+//	console.log("User flipped" + cardThree);
+// } 
+//
